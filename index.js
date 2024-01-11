f4da4de7b038a2192f2463ddbb601f4a3cfdc2d4
totaro <66010583+totaro@users.noreply.github.com>
2024-01-11 17:36:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,88 +1,3 @@
-// const inputEl = document.getElementById("input");
-
-// const infoTextEl = document.getElementById("info-text");
-
-// const meaningContainerEl = document.getElementById("meaning-container");
-
-// const titleEl =  document.getElementById("title");
-// const meaningEl =  document.getElementById("meaning");
-// const audioEl =  document.getElementById("audio");
-
-
-
-// async function fetchAPI(word){
-//     console.log("fetsApi func:", word);
-
-
-//     try {
-
-//         infoTextEl.style.display = "block";
-//         audioEl.style.display = "inline-flex";
-//         meaningContainerEl.style.display = "none";
-        
-//         infoTextEl.innerText = `Searching the meaning of "${word}"`;
-//         const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-
-//         const result = await fetch(url).then((res)=>res.json());
-//         console.log("resultti: ", result);
-
-//         //no word json result
-//         if(result.title){
-//             infoTextEl.style.display = "none";
-//             meaningContainerEl.style.display = "block";
-//             titleEl.innerText = word;
-//             meaningEl.innerText = "N/A"
-//             audioEl.style.display = "none";
-//         } else {
-
-//             infoTextEl.style.display = "none";
-//             meaningContainerEl.style.display = "block";
-//             audioEl.style.display = "inline-flex";
-
-//             titleEl.innerText = result[0].word;
-//             meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
-        
-//             let audioAvailable = result[0].phonetics[1].audio;
-
-//             console.log("audioo",audioAvailable);
-
-//                 if (audioAvailable){
-//                     audioEl.src = result[0].phonetics[1].audio;
-//                 } else {
-//                     audioEl.style.display = "none";
-//                 }
-
-
-//         }
-
-        
-        
-
-        
-//     } catch (error) {
-
-//         infoTextEl.innerText = `An error called "${error.message}" happend, try again later`;
-//         console.log(error);
-        
-//     }
-
-
-    
-
-// }
-
-
-
-
-// inputEl.addEventListener("keyup", (e)=>{
-//     console.log("keyuppia: ", e.key + " " + e.target.value);
-
-//         if (e.target.value && e.key === "Enter"){
-
-//             fetchAPI(e.target.value)
-//         }
-// })
-
 
 /* Header */
 const toggleBtn = document.querySelector('.toggle_btn');
@@ -126,6 +41,8 @@ const dragDemo = document.getElementById( 'drag-and-drop' );
     let droppableLeaveHandler;
     let dropHandler;
 
+    const moved = document.querySelector('.moved');
+
     // childEl.remove();
     dragDemo.style.display = 'block';
 
@@ -151,7 +68,7 @@ const dragDemo = document.getElementById( 'drag-and-drop' );
         
           event.target.classList.remove( 'active' );
           droppable.classList.remove( 'active' );
-          moving.classList.add( 'green' );
+          
         
         };
         
@@ -181,6 +98,11 @@ const dragDemo = document.getElementById( 'drag-and-drop' );
             newCopy = copying.cloneNode( true );
             droppable.appendChild( newCopy );
             newCopy.classList.add( 'green' );
+            newCopy.classList.add( 'copied' );
+            
+            if(newCopy.classList.contains('copied')){
+                newCopy.textContent = "Item copied";
+            }
             copying = null;
         
           }
@@ -190,6 +112,13 @@ const dragDemo = document.getElementById( 'drag-and-drop' );
             droppable.appendChild( moving );
             moving.classList.remove( 'active' );
             moving.classList.add( 'green' );
+            moving.classList.add( 'moved' );
+                        
+            if(moving.classList.contains('moved')){
+                moving.textContent = "Item moved";
+            }
+                       
+            console.log(moving.classList.contains('moved'))
             moving = null;
         
           }
